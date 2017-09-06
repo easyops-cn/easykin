21c484621e58b57f7dcabe3dda9cc9306bd88150
index <indexzhuo@easyops.cn>
2017-09-06 09:36:56 +0800

    增加单元测试

---
+++ tests/unit/HttpTraceTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @author index
+ *   ┏┓   ┏┓+ +
+ *  ┏┛┻━━━┛┻┓ + +
+ *  ┃       ┃
+ *  ┃  ━    ┃ ++ + + +
+ * ████━████┃+
+ *  ┃       ┃ +
+ *  ┃  ┻    ┃
+ *  ┃       ┃ + +
+ *  ┗━┓   ┏━┛
+ *    ┃   ┃
+ *    ┃   ┃ + + + +
+ *    ┃   ┃     Codes are far away from bugs with the animal protecting
+ *    ┃   ┃ +         神兽保佑,代码无bug
+ *    ┃   ┃
+ *    ┃   ┃   +
+ *    ┃   ┗━━━┓ + +
+ *    ┃       ┣┓
+ *    ┃       ┏┛
+ *    ┗┓┓┏━┳┓┏┛ + + + +
+ *     ┃┫┫ ┃┫┫
+ *     ┗┻┛ ┗┻┛+ + + +
+ */
+
+
+class HttpTraceTest extends PHPUnit_Framework_TestCase
+{
+    public function testNewFrontendHttpTrace()
+    {
+        $_SERVER = [
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => '/index.php',
+        ];
+        $trace = new \easyops\easykin\core\HttpTrace();
+        $this->assertEquals(1, $trace->sampled);
+        $this->assertNotNull($trace->serverSpan->traceId);
+        $this->assertNotNull($trace->serverSpan->id);
+        $this->assertNull($trace->serverSpan->parentId);
+    }
+
+    public function testNewBackendHttpTrace()
+    {
+        $_SERVER = [
+            'REQUEST_METHOD' => 'GET',
+            'REQUEST_URI' => '/index.php',
+            'HTTP_X_B3_TRACEID' => 'b839bdf6661687d00a5e0cd93ceb0fdc',
+            'HTTP_X_B3_PARENTSPANID' => '0ac6363774ea1973',
+            'HTTP_X_B3_SPANID' => '0ac6363774ea1973',
+            'HTTP_X_B3_SAMPLED' => '1',
+        ];
+        $trace = new \easyops\easykin\core\HttpTrace();
+        $this->assertEquals(1, $trace->sampled);
+        $this->assertNotNull($trace->serverSpan->traceId);
+        $this->assertNotNull($trace->serverSpan->id);
+        $this->assertNotNull($trace->serverSpan->parentId);
+        $this->assertEquals($_SERVER['HTTP_X_B3_SPANID'], $trace->serverSpan->id);
+    }
+
+}
